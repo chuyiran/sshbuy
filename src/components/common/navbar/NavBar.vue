2f932563ca5add6923a25b4d85c29065167aabe2
chuyiran <252964202@qq.com>
2020-07-03 00:51:21 +0800

    2020-07-03 navbar

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="navbar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+};
+</script>
+
+<style>
+    .navbar{
+        display: flex;
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+        box-shadow: 0px 2px 2px rgba(187, 197, 195, 0.9);
+    }
+    .right,.left{
+        width: 60px;
+    }
+    .center{
+       flex:1;
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,25 @@
 <template>
-<div>
-  这是home主页
+<div id="home">
+  <navbar class="home-navbar">
+    <div slot="center">购物街</div>
+  </navbar>
 </div>
 </template>
 
 <script>
+import navbar from 'components/common/navbar/NavBar'
 export default {
-    name:'home'
+    name:'home',
+    components:{navbar}
 }
 </script>
 
 <style>
-
+  .home-navbar{
+    /* background-color:#ff8189; */
+    background-color: var(--color-tint);
+    color: white;
+    letter-spacing: 2px;
+    font-size: 18px;
+  }
 </style>
