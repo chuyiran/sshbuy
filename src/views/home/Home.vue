4c52e62e073ef3ec4447921a0a6b11818a265ad8
chuyiran <252964202@qq.com>
2020-07-03 11:19:03 +0800

    2020-07-03 banners

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,25 +1,43 @@
 <template>
-<div id="home">
-  <navbar class="home-navbar">
-    <div slot="center">购物街</div>
-  </navbar>
-</div>
+  <div id="home">
+    <navbar class="home-navbar"><div slot="center">购物街</div></navbar>
+    <home-swiper :banners='banners'></home-swiper>
+  </div>
 </template>
 
 <script>
-import navbar from 'components/common/navbar/NavBar'
+import navbar from "components/common/navbar/NavBar";
+import HomeSwiper from './childComponents/HomeSwiper'
+import { getHomeMultiData } from "network/home";
+
 export default {
-    name:'home',
-    components:{navbar}
-}
+  name: "home",
+  components: { navbar, HomeSwiper },
+  data() {
+    return {
+      banners: [],
+      recommend: []
+    };
+  },
+  created() {
+    //生命周期:当组件创建完毕后就会进行网络请求数据
+    //1.多数据请求
+    getHomeMultiData().then(res => {
+      this.banners = res.data.banner.list;
+      this.recommend = res.data.recommend.list;
+      console.log(this.banners);
+      console.log(this.recommend);
+    });
+  }
+};
 </script>
 
 <style>
-  .home-navbar{
-    /* background-color:#ff8189; */
-    background-color: var(--color-tint);
-    color: white;
-    letter-spacing: 2px;
-    font-size: 18px;
-  }
+.home-navbar {
+  /* background-color:#ff8189; */
+  background-color: var(--color-tint);
+  color: white;
+  letter-spacing: 2px;
+  font-size: 18px;
+}
 </style>
